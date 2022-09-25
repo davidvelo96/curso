f222951fd354df1ada7d084aeff70022bbc178b8
davidvelo96 <djimvel554@g.educaand.es>
2022-09-25 12:05:49 +0200

    route

--- ng-route/controller.js
+++ ng-route/controller.js
@@ -1,23 +1,17 @@
 angular
   .module("CustomDirective")
 
-.controller("AppCtrl", function ($scope, $http) {
+.controller("ReposController", function ($scope, $http) {
     $scope.repos = [];
 
     $http.get("https://api.github.com/users/twitter/repos").then(
       function success(data) {
-        // console.log(data);
         var long = data.data;
         $scope.posts = long;
         for (let i = 0; i < long.length; i++) {
             var repo = long[i];
             $scope.repos.push(repo.name);
         }
-        // for (var i = long.length - 1; i >= 0; i--) {
-        //   var repo = long[i];
-        //   $scope.repos.push(repo.name);
-        //   //   console.log(repo.name);
-        // }
       },
       function error(err) {
         console.log(err);
@@ -29,4 +23,17 @@ angular
         $scope.main_repo = data;
       });
     };
+})
+.controller("RepoController",function ($scope,$http,$routeParams) {
+  $scope.repo={};
+  $http.get("https://api.github.com/repos/twitter/"+$routeParams.name).then(
+      function success(data) {
+        var long = data.data;
+        $scope.repo = long;
+     
+      },
+      function error(err) {
+        console.log(err);
+      }
+    );
 });
